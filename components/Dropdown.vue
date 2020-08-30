572bf26d0f22dcbf21418da0ab6f3f28a6ad494a
Lucas Sousa Alves <lucas.salves@terra.com.br>
2020-08-29 23:29:18 -0300

    feature: dropdown open/close animation

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -1,21 +1,27 @@
 <template>
-    <div class="menu-item">
+    <div class="menu-item" @click="isOpen = !isOpen">
         <a href="#">
             {{ title }}
         </a>
-
-        <div class="sub-menu">
-            <div v-for="( item, i ) in items" :key="i" class="menu-item">
-                <a href="item.link">{{ item.title }}</a>
+        <transition name="fade" appear> 
+            <div class="sub-menu" v-if="isOpen">
+                <div v-for="( item, i ) in items" :key="i" class="menu-item">
+                    <a href="item.link">{{ item.title }}</a>
+                </div>
             </div>
-        </div>
+        </transition>
     </div>
  </template>
 
  <script>
      export default {
          name: 'dropdown',
-         props: ['title','items']
+         props: ['title','items'],
+         data(){
+             return{
+                 isOpen: false
+             }
+         }
      }
 </script>
 
