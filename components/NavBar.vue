36666f7a6a96b3ca017fc5e5fbe01db59e420745
Lucas Sousa Alves <lucas.salves@terra.com.br>
2020-08-29 23:03:12 -0300

    style: adding some colour and display

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -13,5 +13,24 @@ export default {
 </script>
 
 <style>
+    nav{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
+    nav .menu-item{
+        color: #FFF;
+        padding: 10px 20px;
+        position: relative;
+        text-align: center;
+        border-bottom: 3px solid transparent;
+        display: flex;
+        transition: .4s;
+    }
+
+    nav .menu-item a {
+        color: inherit;
+        text-decoration: none;
+    }
 </style>
