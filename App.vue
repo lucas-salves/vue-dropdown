a13e619d4057d898391667ad43a0f68aeec8cc16
Lucas Sousa Alves <lucas.salves@terra.com.br>
2020-08-29 22:47:15 -0300

    style: default style setting

--- App.vue
+++ App.vue
@@ -16,5 +16,13 @@ export default {
 </script>
 
 <style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }    
 
+  body{
+    font-family: 'montserrat',sans-serif;
+  }
 </style>
