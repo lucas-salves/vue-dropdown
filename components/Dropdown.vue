a1635bbdd161d90dabf9a2d755403f613f44800d
Lucas Sousa Alves <lucas.salves@terra.com.br>
2020-08-29 23:20:32 -0300

    feature: creating the dropdown structure

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -1,10 +1,21 @@
 <template>
+    <div class="menu-item">
+        <a href="#">
+            {{ title }}
+        </a>
 
+        <div class="sub-menu">
+            <div v-for="( item, i ) in items" :key="i" class="menu-item">
+                <a href="item.link">{{ item.title }}</a>
+            </div>
+        </div>
+    </div>
  </template>
 
  <script>
      export default {
-         name: 'dropdown'
+         name: 'dropdown',
+         props: ['title','items']
      }
 </script>
 
